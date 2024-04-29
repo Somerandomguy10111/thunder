719455a2fe6adce51bba6df8b27f9a3ae2fecbdf
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-29 19:21:04 +0200

    tests: Updated Thunder save/load tests

--- tests/t_save_load.py
+++ tests/t_save_load.py
@@ -1,13 +1,9 @@
 import uuid
-
-import torch
 from holytools.devtools import Unittest
 from tests.mnist import MnistMLP
-from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 
-from thunder.module import ComputeConfigs, SGD, Adam, RunConfigs
+# ---------------------------------------------------------
 
 class TestThunderModel(Unittest):
 
@@ -28,15 +24,13 @@ class TestThunderModel(Unittest):
 
         new = MnistMLP.load(fpath=fpath)
 
-        # new_model = MnistMLP.load(checkpoint_path=save_path)
-        #
-        # keys_to_remove = ['_trainer', '_hparams']
-        # a = {k: v for k, v in original_model.__dict__.items() if k not in keys_to_remove}
-        # b = {k: v for k, v in new_model.__dict__.items() if k not in keys_to_remove}
-        #
-        # print(f'Original state dict: {a}')
-        # print(f'New state dict     : {b}')
-        # self.assertEqual(str(a),str(b))
+        reloadable_keys = ['_trainer', '_hparams','optimizer']
+        original_attr = {k: v for k, v in original.__dict__.items() if k not in reloadable_keys}
+        new_attr = {k: v for k, v in new.__dict__.items() if k not in reloadable_keys}
+
+        print(f'Original state dict: {original_attr}')
+        print(f'New state dict     : {new_attr}')
+        self.assertEqual(str(original_attr),str(new_attr))
 
 if __name__ == "__main__":
     TestThunderModel.execute_all()
