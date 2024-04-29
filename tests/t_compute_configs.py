4e0382946c1088a43b96643a33fd9e87c7e42dea
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-04-29 20:34:41 +0200

    test/compute_configs: Streamlined compute configs testsing'

--- tests/t_compute_configs.py
+++ tests/t_compute_configs.py
@@ -13,29 +13,25 @@ class TestComputeParams(Unittest):
         transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
         cls.mnist_train = datasets.MNIST('/tmp/mnist', train=True, download=True, transform=transform)
         cls.mnist_test = datasets.MNIST('/tmp/mnist', train=False, download=True, transform=transform)
+        cls.run_configs = RunConfigs(epochs=1)
 
     def test_thunder_compute_params(self):
-        compute_configs = ComputeConfigs(num_gpus=1, dtype=torch.float64)
+        compute_configs = ComputeConfigs(accelerator=1, dtype=torch.float64)
         mlp = MnistMLP(compute_configs=compute_configs)
+        print(f'MLP device, dtype = {mlp.device}, {mlp.dtype}')
         self.assertEqual(mlp.dtype, torch.float64)
         self.assertEqual(mlp.device.type, compute_configs.device.type)
 
-
-    def test_training(self):
+    def test_default_training(self):
         mlp = MnistMLP()
-        run_configs = RunConfigs(epochs=1)
-        mlp.train_on(train_data=self.mnist_train, val_data=self.mnist_test, run_configs=run_configs)
+        mlp.train_on(train_data=self.mnist_train, val_data=self.mnist_test, run_configs=self.run_configs)
 
-    #
-    # def test_non_default_training(self):
-    #     compute_configs = ComputeConfigs(num_gpus=1, dtype=torch.float64)
-    #     mlp = MnistMLP(compute_configs=compute_configs)
-    #     print(f'MLP device is {mlp.device}')
-    #     print(f'MLP dtype is {mlp.dtype}')
-    #
-    #     run_configs = RunConfigs(epochs=1)
-    #     mlp.train_on(train_data=self.mnist_train,run_configs=run_configs)
-    #
+
+    def test_non_default_training(self):
+        compute_configs = ComputeConfigs(accelerator=0, dtype=torch.float64)
+        mlp = MnistMLP(compute_configs=compute_configs)
+        mlp.train_on(train_data=self.mnist_train,run_configs=self.run_configs)
+
 
 if __name__ == '__main__':
     TestComputeParams.execute_all()
